a64d01aa89de54e401199253a80348d1b6f20822
Jinrui <jerry.chenj@outlook.com>
2023-05-24 14:13:22 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:caffreygo/caffreygo.github.io.git master
+git push -f git@Jinrui:caffreygo/caffreygo.github.io.git master
 
 cd -
